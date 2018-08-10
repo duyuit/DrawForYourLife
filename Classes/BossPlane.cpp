3450080ab9e32cfc7ebcaa2a9c6b27d909e239ce
Nguyễn Đình Hào <dinhhaobk97@hotmail.com>
2018-08-10 13:50:29 +0700

    fix

--- Classes/BossPlane.cpp
+++ Classes/BossPlane.cpp
@@ -30,7 +30,7 @@ BossPlane::BossPlane()
 
 
 	
-	wing = Sprite::create("Monster/Boss/Wing.png", Rect(0, 0, 408, 36));
+	wing = Sprite::create("Monster/Boss/fan.png", Rect(0, 0, 408, 36));
 	//wing->setAnchorPoint(Vec2(0.91, 0.783));
 	wing->setAnchorPoint(Vec2(0.5,0));
 	wing->setPosition(100, 0);
